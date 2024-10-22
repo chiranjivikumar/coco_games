cd0c31f26d318df66764958af1351da6656fd0e8
Chiranjivi Kumar <145798258+chiranjivikumar@users.noreply.github.com>
2024-10-23 03:02:54 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, num=1;
+    cout<<"Enter your number : ";
+    cin >> n ;
+    while(num<=n){
+        cout<<num<<" ";
+        num++;
+    }
+    cout<<endl;
+    return 0;
+}
