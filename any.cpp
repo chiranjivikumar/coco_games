b0be00455228802aae12997b13464c8417afc6df
Chiranjivi Kumar <145798258+chiranjivikumar@users.noreply.github.com>
2024-11-09 21:50:27 +0530

    Update any.cpp

--- any.cpp
+++ any.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     int n=4;///uiyop[[oipuiop
-//piopiop
-    o;pi;//upoupu[
+//piopiopl;'kl[
+    o;pi;//upoupu[jokyuip
     for(int i=0;i<n;i++){
         char ch = 'A';
         for(int j=0;j<i;j++){
