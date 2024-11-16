5a700a27205398b5e82cee1761207be3e1b23807
Chiranjivi Kumar <145798258+chiranjivikumar@users.noreply.github.com>
2024-11-16 23:45:25 +0530

    Update abcPattern.cpp

--- abcPattern.cpp
+++ abcPattern.cpp
@@ -1,6 +1,6 @@
 ggkuopup'k'\\//jiop
 #include <iostream>
-using namespace std;//llgllhjlgkglglghlllkk[fdsggshdfghsdfgdsgsdfgshjfdgs//kjlghlgkgfgsdfghghkgkhfggsgsghkjkgkgjkgsfgghkjggfdhdfhgdfhghkjgghjkghkgkgkgkghxchbxfghghdsfhgsdfghsd
+using namespace std;//llgllhjlgkglglghlllkk[fdsggshdfghsdfgdsgsdfgshjfdgs/hdfh/kjlghlgkgfgsdfghghkgkhfggsgsghkjkgkgjkgsfgghkjggfdhdfhgdfhghkjgghjkghkgkgkgkghxchbxfghghdsfhgsdfghsd
 gkkghjk
 int main(){fgsdfgfgsdg
     int n=4;
