246b0cea8c1e329e8d9e3a0d5f99ba550450a83e
Chiranjivi Kumar <145798258+chiranjivikumar@users.noreply.github.com>
2024-11-29 19:30:29 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+// function svcet(){
+//     console.log("come labours for attendande")
+// }
+// svcet();
+
+
+// function add(a = 2, b = 4) {
+//     console.log(a + b); 
+//     return a + b;       
+// }
+
+// add();
+
+// 
+
+
+// function add (a=4,b=6){
+    
+//     console.log(a+b);
+// }
+// add();
+
+// consol.log("hello");
+
+// consol.log("welcome");
+
+function mca(){
+    console.log("fucksup");
+}
+function cse(){
+    ece();
+}
+mca();
+
