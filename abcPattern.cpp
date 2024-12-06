e976a1905dd6a49caf82291415d4867a8440e77e
Chiranjivi Kumar <145798258+chiranjivikumar@users.noreply.github.com>
2024-12-06 23:57:50 +0530

    Add files via upload

--- abcPattern.cpp
+++ abcPattern.cpp
@@ -1,11 +1,17 @@
-dhgj
-mfgjh
-d
-a
-aga
-aga
-ag
-aga
-sg
-sgs
-fgh
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+    for(int i=0;i<n;i++){
+        char ch = 'A';
+        for(int j=0;j<i;j++){
+            
+            cout << ch;
+            ch=ch+1;
+            
+        }
+          cout << endl;
+    }
+    return 0;
+}
